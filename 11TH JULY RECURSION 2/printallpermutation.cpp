2bb0d14a316534e9013586fdfed2c1d6b8d8ced4
AdityaMandothia <adityamandothia@gmail.com>
2022-07-12 01:05:25 +0530

    11th JULY Recursion x2

---
+++ 11TH JULY RECURSION 2/printallpermutation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void recpermute(int index, vector<int> &nums, vector<vector<int>> &ans)
+    {
+        if (index == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = index; i < nums.size(); i++)
+        {
+            swap(nums[i], nums[index]);
+            recpermute(index + 1, nums, ans);
+            swap(nums[i], nums[index]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+
+        vector<vector<int>> ans;
+        recpermute(0, nums, ans);
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
