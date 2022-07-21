a0ee2b35b5f639235a6de7dcc5f2c7c3612ce732
AdityaMandothia <adityamandothia@gmail.com>
2022-07-22 00:59:22 +0530

    Binary Tree Striver Sheet 1st day

---
+++ 21st JULY 2022 STRIVER TREES SERIES/traversalPOST.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+public:
+    int data;
+    struct node *left;
+    struct node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void postorder(node *root) // left right root
+{
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+
+    postorder(root->right);
+
+    cout << root->data << " ";
+}
+int main()
+{
+    struct node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    postorder(root);
+
+    return 0;
+}
